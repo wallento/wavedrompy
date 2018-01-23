ecaaf87e3f5a01a6d22da8cb40cf53eafa9463d2
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2018-01-23 22:49:27 +0900

    add default.css as data file

--- setup.py
+++ setup.py
@@ -152,7 +152,10 @@ setup(
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
     #
     # In this case, "data_file" will be installed into "<sys.prefix>/my_data"
-    # data_files=[("my_data", ["data/data_file"])],  # Optional
+    data_files=[
+        # ("my_data", ["data/data_file"]),
+        ("wavedrompy/css", ["css/default.css"]),
+    ],  # Optional
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
