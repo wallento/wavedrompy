caab1cb08d50bfd2dd8885c8b29ea5534f26341d
Kazuki Yamamoto <k.yamamoto.08136891@gmail.com>
2018-01-28 13:27:39 +0900

    fix runtime error

--- wavedrompy/waveskin.py
+++ wavedrompy/waveskin.py
@@ -1,4 +1,4 @@
-import css
+from . import css
 WaveSkin = {}
 
 WaveSkin['default'] = ["svg", {"id": "svg", "xmlns": "http://www.w3.org/2000/svg", "xmlns:xlink": "http://www.w3.org/1999/xlink", "height": "0"},
