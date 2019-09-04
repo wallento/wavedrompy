510e690ccbe3143a10c59e800c3d42a35c7563f5
Stefan Wallentowitz <stefan@wallentowitz.de>
2019-09-04 23:24:51 +0200

    Split brick regressions into individual tests

--- test/test_brick_regression.py
+++ test/test_brick_regression.py
@@ -4,18 +4,14 @@ from wavedrom import WaveDrom
 from brick_regressions import all
 
 
-@pytest.fixture
-def make_regression():
-    def _make_regression(test):
-        w = WaveDrom()
-        w.lane.period = test.period
-        w.lane.hscale = test.hscale
-        w.lane.phase = test.phase
-        output = w.parse_wave_lane(test.wave, test.period * test.hscale - 1)
-        assert(output == test.expected)
-    return _make_regression
+def pytest_generate_tests(metafunc):
+    metafunc.parametrize("test", all)
 
 
-def test_regression(make_regression):
-    for test in all:
-        make_regression(test)
+def test_regression(test):
+    w = WaveDrom()
+    w.lane.period = test.period
+    w.lane.hscale = test.hscale
+    w.lane.phase = test.phase
+    output = w.parse_wave_lane(test.wave, test.period * test.hscale - 1)
+    assert(output == test.expected)
