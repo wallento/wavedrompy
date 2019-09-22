b32b54c12003566f0c4f8e18b20973fa440504eb
MutantPlatypus <1389865+MutantPlatypus@users.noreply.github.com>
2019-09-22 18:44:39 -0400

    Declare support for Python 3.7, test in 3.7

--- setup.py
+++ setup.py
@@ -107,6 +107,7 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7"
     ],
 
     # This field adds keywords for your project which will appear on the
